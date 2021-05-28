6740bf8cd87527eaf071472d749355a1a47d4223
RiseT <12624674+RiseT@users.noreply.github.com>
2021-05-28 21:55:21 +0200

    Format black .py files

--- Chapter 6 Pandas/pandas_samples.py
+++ Chapter 6 Pandas/pandas_samples.py
@@ -2,15 +2,14 @@ import pandas as pd
 
 data = pd.read_csv("spy.csv", index_col=0, parse_dates=[0])
 
-data['pct'] = data['Close'].pct_change()
+data["pct"] = data["Close"].pct_change()
 
-start = '2016/5/5'
-end = '2017'
+start = "2016/5/5"
+end = "2017"
 
 
 data_window = data[start:end]
 
-min_pct = data_window['pct'].min()
-max_pct = data_window['pct'].max()
-std = data_window['pct'].std()
-
+min_pct = data_window["pct"].min()
+max_pct = data_window["pct"].max()
+std = data_window["pct"].std()
